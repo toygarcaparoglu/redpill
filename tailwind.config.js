e5a5f67198f720ad49c1faac04486f4d04e6c5c2
Toygar <toygar.caparoglu@gmail.com>
2025-06-21 22:11:05 +0300

    apparence

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./layouts/**/*.html", "./content/**/*.md"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./themes/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
